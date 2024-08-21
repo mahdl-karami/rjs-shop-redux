75b3be962ffd51afc5eb6806934b3710ff5ff341
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-08-21 12:05:59 +0330

    fetch products in store

--- src/app/features/products/productsSlice.js
+++ src/app/features/products/productsSlice.js
@@ -2,15 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   allProducts: undefined,
+  loading: false,
+  eroor: false,
 };
 
 export const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {
-    sendig: (state) => {},
+    sending: (state) => {
+      state.loading = true;
+    },
+    success: (state, { payload }) => {
+      state.allProducts = payload;
+      state.loading = false;
+    },
   },
 });
 
-export const { sendig } = productsSlice.actions;
+export const { sending, success } = productsSlice.actions;
 export default productsSlice.reducer;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,37 +1,49 @@
 //? import hooks
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 //? import services
 import products from "../services/products";
 //? import components
 import ProductCard from "../components/ProductCard";
+import { sending, success } from "../app/features/products/productsSlice";
 
 function Products() {
-  console.log("first")
+  const { loading, allProducts } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  console.log(loading);
   //! fetch products
   useEffect(() => {
-    // if (!visibleProducts) {
-    products()
-      .get()
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => console.log(err));
-    // }
+    if (!allProducts) {
+      dispatch(sending());
+      products()
+        .get()
+        .then((res) => {
+          dispatch(success(res.data));
+        })
+        .catch((error) => console.log(err));
+    }
   }, []);
   //! jsx
-  // if (loading) return <h1>loading</h1>;
+  if (loading) return <h1>loading</h1>;
   // if (error) return <h1>{error.message}</h1>;
-  // if (visibleProducts)
-  return (
-    <div className="products">
-      products
-      {/* {visibleProducts.map((product, index) => (
+  if (allProducts) {
+    console.log(allProducts);
+    return (
+      <div className="products">
+        {allProducts.map((product, index) => (
+          <div key={index} className="product">
+            {product.title}
+            {/* <ProductCard product={product} allProducts={visibleProducts} dispatch={dispatch} /> */}
+          </div>
+        ))}
+        {/* {visibleProducts.map((product, index) => (
         <div key={index} className="product">
           <ProductCard product={product} allProducts={visibleProducts} dispatch={dispatch} />
         </div>
       ))} */}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Products;
